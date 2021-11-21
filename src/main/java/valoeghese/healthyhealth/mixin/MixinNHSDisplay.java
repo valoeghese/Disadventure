2bc99820ddf57c02b0519a085cedab58e71533c3
valoeghese <valoeghesemods@gmail.com>
2021-11-21 20:26:14 +1300

    Better commit thi

---
+++ src/main/java/valoeghese/healthyhealth/mixin/MixinNHSDisplay.java
@@ -0,0 +1,12 @@
+package valoeghese.healthyhealth.mixin;
+
+import net.minecraft.client.gui.screens.Overlay;
+import net.minecraft.world.food.FoodData;
+
+
+/**
+ * Mixin into the minecraft-national health system display.
+ */
+@Mixin(FoodData.class)
+public class MixinNHSDisplay {
+}
