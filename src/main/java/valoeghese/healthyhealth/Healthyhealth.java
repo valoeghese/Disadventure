25c07c3ba458abe6dfa39a04cee7f7191647d6c9
valoeghese <valoeghesemods@gmail.com>
2021-11-21 15:36:44 +1300

    iuibeilbelvebvulbwe vulybv

---
+++ src/main/java/valoeghese/healthyhealth/Healthyhealth.java
@@ -0,0 +1,17 @@
+package valoeghese.healthyhealth;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import java.util.Random;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Healthyhealth implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger("Healthyhealth");
+
+	@Override
+	public void onInitialize() {
+		// Add init code here!
+	}
+}
